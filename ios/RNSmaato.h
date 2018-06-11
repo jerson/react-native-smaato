92c828f2214ed4a620bb12942369453a0c6692ba
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-06-10 20:26:44 -0500

    fixed ios

--- ios/RNSmaato.h
+++ ios/RNSmaato.h
@@ -1,18 +1,12 @@
 
 
-#if __has_include("RCTEventEmitter.h")
-#import "RCTEventEmitter.h"
-#else
-#import <React/RCTEventEmitter.h>
-#endif
-
 #if __has_include("RCTViewManager.h")
 #import "RCTViewManager.h"
 #else
 #import <React/RCTViewManager.h>
 #endif
 
-@interface RNSmaato : RCTEventEmitter <RCTBridgeModule>
+@interface RNSmaato : NSObject <RCTBridgeModule>
 
 @end
   
